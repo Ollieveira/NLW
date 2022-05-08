7cbef3a3e6a3833f0eaed5e00b1ac786233e648f
Willys de Oliveira <willys_oliveira@hotmail.com>
2022-05-08 00:48:11 -0300

    Ultima Modificação
    -Foi atualizado no CSS para que a pagina seja visivel para qualquer tipo de tela.

--- main.js
+++ main.js
@@ -3,11 +3,40 @@ window.addEventListener('scroll', onScroll)
 onScroll()
 
 function onScroll() {
-  showNavOnScroll ()
-  showBackToTopButtonOnScroll ()
+  showNavOnScroll()
+  showBackToTopButtonOnScroll()
+  activateMenuAtCurrentSection(home)
+  activateMenuAtCurrentSection(services)
+  activateMenuAtCurrentSection(about)
+  activateMenuAtCurrentSection(contact)
 }
 
-function showNavOnScroll (){
+function activateMenuAtCurrentSection(section) {
+  const targetLine = scrollY + innerHeight / 2
+
+  //Verificar se a seção passou da linha
+  const sectionTop = section.offsetTop
+  const sectionHeight = section.offsetHeight
+  const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop
+
+  //Verificar se a base está abaixo da linha alvo
+  const sectionEndsAt = sectionTop + sectionHeight
+  const sectionEndPassedTargetLine = sectionEndsAt <= targetLine
+
+  //Limites da seção
+  const sectionBoundaries =
+    sectionTopReachOrPassedTargetLine && !sectionEndPassedTargetLine
+
+  const sectionId = section.getAttribute('id')
+  const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
+
+  menuElement.classList.remove('active')
+  if (sectionBoundaries) {
+    menuElement.classList.add('active')
+  }
+}
+
+function showNavOnScroll() {
   if (scrollY > 0) {
     navigation.classList.add('scroll')
   } else {
@@ -15,7 +44,7 @@ function showNavOnScroll (){
   }
 }
 
-function showBackToTopButtonOnScroll (){
+function showBackToTopButtonOnScroll() {
   if (scrollY > 400) {
     backToTopButton.classList.add('show')
   } else {
